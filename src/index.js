56ade3d7a5b12fae86d05c7cb2a4b9b3e8ad9274
martinafarah6 <martinafarah6@gmail.com>
2026-09-22 18:46:16 +0300

    Initialize React application entry point

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import ReactDom from 'react-dom/client';
+import React from 'react';
+import App from './App';
+
+const root=ReactDom.createRoot(document.getElementById("root"));
+root.render(
+    <>
+        <App/>
+    </>
+)
