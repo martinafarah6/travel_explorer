7a0f8a9e0342e0241d20cebb35a78724bfa418bb
martinafarah6 <martinafarah6@gmail.com>
2026-09-22 18:49:27 +0300

    Add Footer component with social media links

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import './Footer.css';
+export default function Footer()
+{
+    return(
+        <footer>
+            <ul>
+                <li><a href="https://www.facebook.com/?utm_source=chatgpt.com"><img src="https://i.pinimg.com/736x/ab/96/4b/ab964bcf79cbc2d86b15a9e2efe05ffa.jpg" alt="FaceBook" width="30px"/></a></li>
+                <li><a href="https://wa.me/?utm_source=chatgpt.com"><img src="https://i.pinimg.com/736x/28/af/d7/28afd71d9474a014395faca3c7c6edab.jpg" alt="WhatsApp" width="30px"/></a></li>
+                <li><a href="https://www.instagram.com/?utm_source=chatgpt.com"><img src="https://i.pinimg.com/1200x/e8/00/48/e80048be84cfe35cc89dda88c1fd647c.jpg" alt="Insta" width="30px"/></a></li>
+                <li><a href="https://www.linkedin.com/?utm_source=chatgpt.com"><img src="https://i.pinimg.com/736x/b5/89/56/b58956b46a5198db7e10f5a7b02ec1d7.jpg" alt="LinkedIn" width="30px"/></a></li>
+            </ul>
+            <p>@Travel Explorer.All rights reserved.</p>
+        </footer>
+    )
+}
